47ce9f4b436f2b5eac22bab514862a2aac20437f
Anuj Upadhyaya <=>
2021-12-27 21:59:31 +0530

    Added responsiveness

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+    const allNavItems = document.getElementsByClassName('nav-item');
     const cards = document.getElementsByClassName("card")
     for(let i=0; i<cards.length; i++) {
         const len = cards[i].innerHTML.length
@@ -35,35 +36,105 @@ $(document).ready(() => {
         $('html,body').animate({
             scrollTop: $(".profile-name-container").offset().top - offsetY},
             'slow');
+        if(!allNavItems[0].classList.contains('active')){
+            $(this).addClass('active');
+        }
+        removeActiveClass(0);
     });
 
     $('.nav-about-me').on('click', function() {
         $('html,body').animate({
             scrollTop: $("#about").offset().top - offsetY},
             'slow');
+        if(!allNavItems[1].classList.contains('active')){
+            $(this).addClass('active');
+        }
+        removeActiveClass(1);
     });
 
     $('.nav-skills').on('click', function() {
         $('html,body').animate({
             scrollTop: $("#skills").offset().top - offsetY},
             'slow');
+        if(!allNavItems[2].classList.contains('active')){
+            $(this).addClass('active');
+        }
+        removeActiveClass(2);
     });
 
     $('.nav-experience').on('click', function() {
         $('html,body').animate({
             scrollTop: $("#experience").offset().top - offsetY},
             'slow');
+        if(!allNavItems[3].classList.contains('active')){
+            $(this).addClass('active');
+        }
+        removeActiveClass(3);
     });
 
     $('.nav-education').on('click', function() {
         $('html,body').animate({
             scrollTop: $("#education").offset().top - offsetY},
             'slow');
+        if(!allNavItems[4].classList.contains('active')){
+            $(this).addClass('active');
+        }
+        removeActiveClass(4);
     });
 
     $('.nav-contact').on('click', function() {
         $('html,body').animate({
             scrollTop: $(".footer").offset().top},
             'slow');
+        if(!allNavItems[5].classList.contains('active')){
+            $(this).addClass('active');
+        }
+        removeActiveClass(5);
     });
-}) 
+
+    function removeActiveClass(index) {
+        for(let i=0; i<allNavItems.length; i++) {
+            if(i!==index && allNavItems[i].classList.contains('active')) {
+                switch(i) {
+                    case 0:
+                        $('.nav-home').removeClass('active')
+                        break;
+                    case 1:
+                        $('.nav-about-me').removeClass('active')
+                        break;
+                    case 2:
+                        $('.nav-skills').removeClass('active')
+                        break;
+                    case 3:
+                        $('.nav-experience').removeClass('active')
+                        break;
+                    case 4:
+                        $('.nav-education').removeClass('active')
+                        break;
+                    case 5:
+                        $('.nav-contact').removeClass('active')
+                        break;
+                }
+            }
+        }
+    }
+
+    function responsive(maxWidth) {
+        if (maxWidth.matches) { 
+            $('.profile-image-1').removeClass('hidden');
+            $('.profile-image-2').addClass('hidden');
+            $('.small-screen').removeClass('hidden');
+            $('.large-screen').addClass('hidden');
+            console.log('called')
+        } else {
+            $('.profile-image-1').addClass('hidden');
+            $('.profile-image-2').removeClass('hidden');
+            $('.small-screen').addClass('hidden');
+            $('.large-screen').removeClass('hidden');
+        }
+      }
+    var maxWidth = window.matchMedia("(max-width: 500px)");
+    
+    responsive(maxWidth);
+    maxWidth.addEventListener("change", responsive);
+})
